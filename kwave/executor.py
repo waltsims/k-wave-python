8c3ff08de8f3c4ab80ffbb93efa21bc198db9a69
ktritz <ktritz@gmail.com>
2022-07-22 23:13:33 -0400

    Added Windows compatability

--- kwave/executor.py
+++ kwave/executor.py
@@ -14,11 +14,6 @@ class Executor:
     def __init__(self, device):
         self._is_linux = sys.platform.startswith('linux')
 
-        if not self._is_linux:
-            raise NotImplementedError('Running on Windows and Mac is not implemented yet.'
-                                      ' Please open an issue on Github:'
-                                      ' https://github.com/waltsims/k-wave-python/issues/new')
-
         binary_folder = 'linux' if self._is_linux else 'windows'
         binary_name = 'kspaceFirstOrder'
         if device == 'gpu':
@@ -27,6 +22,9 @@ class Executor:
             binary_name += '-OMP'
         else:
             raise ValueError("Unrecognized value passed as target device. Options are 'gpu' or 'cpu'.")
+        
+        if binary_folder == 'windows':
+            binary_name += '.exe'
 
         path_of_this_file = Path(__file__).parent.resolve()
         self.binary_path = path_of_this_file / 'bin' / binary_folder / binary_name
@@ -37,12 +35,14 @@ class Executor:
         self.binary_path.chmod(self.binary_path.stat().st_mode | stat.S_IEXEC)
 
     def run_simulation(self, input_filename: str, output_filename: str, options: str):
-        env_variables = 'export LD_LIBRARY_PATH=;' \
-                        'OMP_PLACES=cores;' \
-                        'OMP_PROC_BIND=SPREAD;'
-
-        command = f'{env_variables} {self.binary_path} ' \
-                  f'-i {input_filename} -o {output_filename} {options}'
+        env_variables = {
+            'LD_LIBRARY_PATH': '',
+            'OMP_PLACES': 'cores',
+            'OMP_PROC_BIND': 'SPREAD',
+        }
+        os.environ.update(env_variables)
+        
+        command = f'{self.binary_path} -i {input_filename} -o {output_filename} {options}'
 
         return_code = os.system(command)
 
