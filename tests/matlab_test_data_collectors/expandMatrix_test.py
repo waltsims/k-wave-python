a5b18efc9be34f03d1593274270502a84ddfb722
Walter Simson <walter.simson@tum.de>
2022-07-23 12:54:25 -0700

    Add skip for test with missing ref files

--- tests/matlab_test_data_collectors/expandMatrix_test.py
+++ tests/matlab_test_data_collectors/expandMatrix_test.py
@@ -5,30 +5,32 @@ from kwave.utils import min_nd
 from scipy.io import loadmat
 import numpy as np
 import os
-
-collected_values_folder = '/data/code/Work/black_box_testing/collectedValues_expandMatrix'
-num_collected_values = len(os.listdir(collected_values_folder))
+import pytest
 
 
-for i in range(num_collected_values):
-    filepath = os.path.join(collected_values_folder, f'{i:06d}.mat')
-    recorded_data = loadmat(filepath)
+@pytest.mark.skip(reason="Reference files to not always exist.")
+def test_expand_matrix_test():
+    collected_values_folder = '/data/code/Work/black_box_testing/collectedValues_expandMatrix'
+    num_collected_values = len(os.listdir(collected_values_folder))
+    for i in range(num_collected_values):
+        filepath = os.path.join(collected_values_folder, f'{i:06d}.mat')
+        recorded_data = loadmat(filepath)
 
-    matrix = recorded_data['matrix']
-    matrix = np.squeeze(matrix)
-    input_args = recorded_data['input_args']
-    exp_coeff = input_args[0][0]
-    if len(input_args[0]) == 2:
-        edge_val = float(input_args[0][1])
-    else:
-        edge_val = None
-    expected_expanded_matrix = recorded_data['expanded_matrix']
-    expected_expanded_matrix = np.squeeze(expected_expanded_matrix)
+        matrix = recorded_data['matrix']
+        matrix = np.squeeze(matrix)
+        input_args = recorded_data['input_args']
+        exp_coeff = input_args[0][0]
+        if len(input_args[0]) == 2:
+            edge_val = float(input_args[0][1])
+        else:
+            edge_val = None
+        expected_expanded_matrix = recorded_data['expanded_matrix']
+        expected_expanded_matrix = np.squeeze(expected_expanded_matrix)
 
-    expanded_matrix  = expand_matrix(matrix, exp_coeff=exp_coeff, edge_val=edge_val)
+        expanded_matrix  = expand_matrix(matrix, exp_coeff=exp_coeff, edge_val=edge_val)
 
-    print(i)
-    assert np.allclose(expected_expanded_matrix, expanded_matrix, equal_nan=True)
+        print(i)
+        assert np.allclose(expected_expanded_matrix, expanded_matrix, equal_nan=True)
 
 
-print('expanded_matrix(..) works as expected!')
+    print('expanded_matrix(..) works as expected!')
--- tests/matlab_test_data_collectors/maxND_test.py
+++ tests/matlab_test_data_collectors/maxND_test.py
@@ -3,23 +3,27 @@ from kwave.utils import max_nd
 from scipy.io import loadmat
 import numpy as np
 import os
-
-collected_values_folder = '/data/code/Work/black_box_testing/collectedValues_maxND'
-num_collected_values = len(os.listdir(collected_values_folder))
+import pytest
 
 
-for i in range(num_collected_values):
-    filepath = os.path.join(collected_values_folder, f'{i:06d}.mat')
-    recorded_data = loadmat(filepath)
-
-    matrix = recorded_data['matrix']
-    expected_max_val = float(recorded_data['max_val'])
-    expected_ind = np.squeeze(recorded_data['ind'])
-
-    max_val, ind = max_nd(matrix)
-
-    assert np.allclose(expected_max_val, max_val, equal_nan=True)
-    assert np.allclose(expected_ind, ind)
+@pytest.mark.skip(reason="Reference files to not always exist.")
+def test_maxND():
+    collected_values_folder = '/data/code/Work/black_box_testing/collectedValues_maxND'
+    num_collected_values = len(os.listdir(collected_values_folder))
 
 
-print('max_nd(..) works as expected!')
+    for i in range(num_collected_values):
+        filepath = os.path.join(collected_values_folder, f'{i:06d}.mat')
+        recorded_data = loadmat(filepath)
+
+        matrix = recorded_data['matrix']
+        expected_max_val = float(recorded_data['max_val'])
+        expected_ind = np.squeeze(recorded_data['ind'])
+
+        max_val, ind = max_nd(matrix)
+
+        assert np.allclose(expected_max_val, max_val, equal_nan=True)
+        assert np.allclose(expected_ind, ind)
+
+
+    print('max_nd(..) works as expected!')
