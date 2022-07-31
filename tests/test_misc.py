0ff279d0cec77132ac40a673a0157ddff9b0f183
Walter Simson <walter.simson@tum.de>
2022-07-30 21:35:58 -0700

    Add focused bowl O'Neil - test point-wise from example

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,5 +1,4 @@
-from kwave.utils.misc import round_even, round_odd, find_closest
-
+from kwave.utils.misc import round_even, round_odd, find_closest, focused_bowl_oneil
 
 import numpy as np
 
@@ -26,3 +25,38 @@ def test_find_closest():
     assert a_close == 2
     assert idx_close == (1,)
 
+
+def test_focused_bowl_oneil():
+    # define transducer parameters
+    radius = 140e-3  # [m]
+    diameter = 120e-3  # [m]
+    velocity = 100e-3  # [m / s]
+    frequency = 1e6  # [Hz]
+    sound_speed = 1500  # [m / s]
+    density = 1000  # [kg / m ^ 3]
+
+    # define position vectors
+    axial_position = np.arange(0, 250e-3 + 1e-4, 1e-4)  # [m]
+    lateral_position = np.arange(-15e-3, 15e-3 + 1e-4, 1e-4)  # [m]
+
+    # evaluate pressure
+    [p_axial, p_lateral] = focused_bowl_oneil(radius, diameter,
+                                              velocity, frequency, sound_speed, density,
+                                              axial_position, lateral_position)
+    # compare first and last vals
+    assert np.isclose(p_axial[0], 1227.01736354) and np.isclose(p_axial[-1], 13036.24465513)
+    assert np.isclose(p_lateral[0], 82805.42312207383) and np.isclose(p_lateral[-1], 82805.42312207383)
+    assert np.isclose(p_lateral[5], 12880.47179330867)  # one final test value
+
+    # Test passing one arg as none
+    [_, p_lateral] = focused_bowl_oneil(radius, diameter,
+                                        velocity, frequency, sound_speed, density,
+                                        axial_positions=axial_position)
+    assert p_lateral is None
+
+    # Test passing the other arg as none
+    [p_axial, _] = focused_bowl_oneil(radius, diameter,
+                                      velocity, frequency, sound_speed, density,
+                                      lateral_positions=lateral_position)
+
+    assert p_axial is None
