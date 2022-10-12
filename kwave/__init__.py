33d7cccb4bac70b20074d7aad09de2919754809b
Walter Simson <waltersimson@stanford.edu>
2022-10-12 11:42:53 -0700

    Add hatch build system

--- kwave/__init__.py
+++ kwave/__init__.py
@@ -1,4 +1,5 @@
 import os
+from os import environ
 import sys
 
 from kwave.kgrid import kWaveGrid
@@ -6,10 +7,11 @@ from kwave.kmedium import kWaveMedium
 from kwave.options import SimulationOptions
 from kwave.ktransducer import NotATransducer
 
+VERSION = '0.1.0'
 # Set environment variable to binaries to get rid of user warning
 # This code is a crutch and should be removed when kspaceFirstOrder
 # is refactored
-from os import environ
+
 if sys.platform.startswith('darwin'):
     system = "macOS"
 elif sys.platform.startswith('linux'):
