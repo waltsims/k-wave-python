8bc2095e5caea0ed90094bdcf5a7e7529e90af3f
Walter Simson <waltersimson@stanford.edu>
2022-12-29 17:26:53 -0800

    split sharpness tests

--- tests/matlab_test_data_collectors/python_testers/sharpness_test.py
+++ tests/matlab_test_data_collectors/python_testers/sharpness_test.py
@@ -7,7 +7,7 @@ from kwave.utils.filters import sharpness
 from tests.matlab_test_data_collectors.python_testers.utils.record_reader import TestRecordReader
 
 
-def test_tennenbaum():
+def test_tennenbaum2d():
     test_record_path = os.path.join(Path(__file__).parent, Path(
         'collectedValues/tenenbaum.mat'))
     reader = TestRecordReader(test_record_path)
@@ -18,13 +18,19 @@ def test_tennenbaum():
     out_prime = reader.expected_value_of('out2')
     assert np.allclose(out, out_prime), "Tenenbaum sharpness did not match expected sharpness in 2D"
 
+
+def test_tennenbaum3d():
+    test_record_path = os.path.join(Path(__file__).parent, Path(
+        'collectedValues/tenenbaum.mat'))
+    reader = TestRecordReader(test_record_path)
+
     img = reader.expected_value_of('img3')
     out = sharpness(img, 'Tenenbaum')
     out_prime = reader.expected_value_of('out3')
     assert np.allclose(out, out_prime), "Tenenbaum sharpness did not match expected sharpness in 3D"
 
 
-def test_brenner():
+def test_brenner2d():
     test_record_path = os.path.join(Path(__file__).parent, Path(
         'collectedValues/brenner.mat'))
     reader = TestRecordReader(test_record_path)
@@ -33,6 +39,12 @@ def test_brenner():
 
     out_prime = reader.expected_value_of('out2')
     assert np.allclose(out, out_prime), "Brenner sharpness did not match expected sharpness in 2D"
+
+
+def test_brenner3d():
+    test_record_path = os.path.join(Path(__file__).parent, Path(
+        'collectedValues/brenner.mat'))
+    reader = TestRecordReader(test_record_path)
 
     img = reader.expected_value_of('img3')
     out = sharpness(img, 'Brenner')
