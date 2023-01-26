511607a7cb07e0d1088fb361ca911b99197fe021
Walter Simson <waltersimson@stanford.edu>
2023-01-26 14:55:24 -0800

    Add spaces to EOFs

--- tests/matlab_test_data_collectors/matlab_collectors/collect_normvar.m
+++ tests/matlab_test_data_collectors/matlab_collectors/collect_normvar.m
@@ -10,4 +10,4 @@ img3 = rand([5,5,5]);
 out3 = sharpness(img3, 'NormVariance');
 recorder.recordVariable('img3', img3);
 recorder.recordVariable('out3', out3);
-recorder.saveRecordsToDisk();    
+recorder.saveRecordsToDisk();
