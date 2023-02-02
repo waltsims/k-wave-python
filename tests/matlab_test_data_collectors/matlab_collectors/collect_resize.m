fbdc6db2e2096a3ad2d2a8a8b1cf5f6685af84ec
Walter Simson <waltersimson@stanford.edu>
2023-02-01 21:38:59 -0800

    Test resize with RecordRecorder

--- tests/matlab_test_data_collectors/matlab_collectors/collect_resize.m
+++ tests/matlab_test_data_collectors/matlab_collectors/collect_resize.m
@@ -1,4 +1,8 @@
 % Generate a list of interpolation methods
+
+output_file = 'collectedValues/resize.mat';
+recorder = utils.TestRecorder(output_file);
+
 % TODO: test also for nearest
 % interp_methods = {'nearest', 'linear'};
 interp_methods = {'linear'};
@@ -12,20 +16,18 @@ volume = rand([Nx, Ny, Nz]);
 % Specify the desired new size of the volume
 new_size = [randi([10,20]), randi([10,20]), randi([10,20])];
 
-output_folder = 'collectedValues/resize/';
-k = 0;
 
 % Loop through each interpolation method
 for method = interp_methods
     % Use the current interpolation method to resize the volume
     resized_volume = resize(volume, new_size, char(method));
 
-    % Save the original volume, resized volume, and interpolation method
-    if ~exist(output_folder, 'dir')
-        mkdir(output_folder);
-    end
-    idx_padded = sprintf('%06d', k );
-    filename = [output_folder idx_padded '.mat'];
-    save(filename, 'volume', 'resized_volume', 'new_size', 'method');
-    k = k + 1;
+    recorder.recordVariable('volume', volume);
+    recorder.recordVariable('resized_volume', resized_volume);
+    recorder.recordVariable('new_size', new_size);
+    recorder.recordVariable('method', method)
+    recorder.increment();
+
 end
+recorder.saveRecordsToDisk(); 
+    
--- tests/matlab_test_data_collectors/python_testers/test_resize.py
+++ tests/matlab_test_data_collectors/python_testers/test_resize.py
@@ -2,28 +2,24 @@ import os
 from pathlib import Path
 
 import numpy as np
-from scipy.io import loadmat
 
 from kwave.utils.matrix import resize
+from tests.matlab_test_data_collectors.python_testers.utils.record_reader import TestRecordReader
 
 
 def test_resize():
-    collected_values_folder = os.path.join(Path(__file__).parent, 'collectedValues/resize')
+    test_record_path = os.path.join(Path(__file__).parent, 'collectedValues/resize.mat')
+    reader = TestRecordReader(test_record_path)
 
-    num_collected_values = len(os.listdir(collected_values_folder))
+    for i in range(len(reader)):
+        volume = reader.expected_value_of('volume')
+        expected_resized_volume = reader.expected_value_of('resized_volume')
+        new_size = reader.expected_value_of('new_size')
+        method = reader.expected_value_of('method')  # TODO: does not work for spline cases
 
-    for i in range(num_collected_values):
-        print(i)
-        filepath = os.path.join(collected_values_folder, f'{i:06d}.mat')
-        recorded_data = loadmat(filepath)
-        # 'params', 'volume', 'resized_volume', 'new_size'
-        mat = recorded_data['volume']
-        expected_mat = recorded_data['resized_volume'].squeeze()
-        new_size = recorded_data['new_size'][0]
-        method = recorded_data['method'][0]  # TODO: does not work for spline cases
+        resized_volume = resize(volume, new_size, interp_mode=method)
 
-        resized_mat = resize(mat, new_size, interp_mode=method)
-
-        assert np.allclose(expected_mat, resized_mat), f"Results do not match for {i + 1} dimensional case."
+        assert np.allclose(expected_resized_volume,
+                           resized_volume), f"Results do not match for {i + 1} dimensional case."
 
     print('revolve2d(..) works as expected!')
