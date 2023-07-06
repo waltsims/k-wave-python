d7c6b82d77e4ab75d9b30e16a55e4896a50c5e26
Walter Simson <waltersimson@stanford.edu>
2023-07-05 21:20:52 -0700

    Add make_cart_spherical_segment and test

---
+++ tests/matlab_test_data_collectors/python_testers/makeCartSphericalSegment_test.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+import numpy as np
+
+from kwave.utils.mapgen import make_cart_spherical_segment
+from tests.matlab_test_data_collectors.python_testers.utils.record_reader import TestRecordReader
+
+
+def test_make_cart_spherical_segments():
+    test_record_path = os.path.join(Path(__file__).parent, 'collectedValues/makeCartSphericalSegment.mat')
+    reader = TestRecordReader(test_record_path)
+
+    for i in range(len(reader)):
+        params = reader.expected_value_of('params')
+        bowl_pos, radius, inner_diameter, outer_diameter, focus_pos, num_points, plot_bowl, num_points_inner = params
+
+        coordinates = make_cart_spherical_segment(bowl_pos, radius, inner_diameter, outer_diameter, focus_pos,
+                                                  num_points, plot_bowl, num_points_inner)
+        assert np.allclose(coordinates, reader.expected_value_of('coordinates'), equal_nan=True)
+        reader.increment()
+
+    print('make_cart_bowl(..) works as expected!')
