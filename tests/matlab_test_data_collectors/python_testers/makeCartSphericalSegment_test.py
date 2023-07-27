85ab1509ae3943624ff6af0f1026ec48afc1273c
Walter Simson <walter.simson@tum.de>
2023-07-27 11:43:28 -0700

    Update logging message

--- tests/matlab_test_data_collectors/python_testers/makeCartSphericalSegment_test.py
+++ tests/matlab_test_data_collectors/python_testers/makeCartSphericalSegment_test.py
@@ -20,4 +20,4 @@ def test_make_cart_spherical_segments():
         assert np.allclose(coordinates, reader.expected_value_of('coordinates'), equal_nan=True)
         reader.increment()
 
-    print('make_cart_bowl(..) works as expected!')
+    print('make_cart_spherical_segment(..) works as expected!')
