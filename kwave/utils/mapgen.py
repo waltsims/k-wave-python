4b0052ae7d4af1a5c40f2fb94c04eb6cc314447a
Walter Simson <walter.simson@tum.de>
2023-07-27 12:00:48 -0700

    Add make_cart_bowl (#164)
    * updated makeCartBowl
    
    ---------
    
    Co-authored-by: Ameya Deshmukh <ameyaudeshmukh@gmail.com>

--- kwave/utils/mapgen.py
+++ kwave/utils/mapgen.py
@@ -10,11 +10,96 @@ from scipy import optimize
 
 from .conversion import db2neper, neper2db
 from .data import scale_SI
-from .math import cosd, sind, Rz, Ry, Rx
+from .math import cosd, sind, Rz, Ry, Rx, compute_linear_transform
 from .matlab import matlab_assign, matlab_find, ind2sub, sub2ind
 from .matrix import max_nd
 from .tictoc import TicToc
 from ..data import Vector
+
+
+def make_cart_bowl(bowl_pos: np.ndarray, radius: float, diameter: float, focus_pos: np.ndarray, num_points: int,
+                   plot_bowl: Optional[bool] = False) -> np.ndarray:
+    """
+    Create evenly distributed Cartesian points covering a bowl.
+
+    Args:
+        bowl_pos:       Cartesian position of the centre of the rear surface of
+                        the bowl given as a three element vector [bx, by, bz] [m].
+        radius:         Radius of curvature of the bowl [m].
+        diameter:       Diameter of the opening of the bowl [m].
+        focus_pos:      Any point on the beam axis of the bowl given as a three
+                        element vector [fx, fy, fz] [m].
+        num_points:     Number of points on the bowl.
+        plot_bowl:      Boolean controlling whether the Cartesian points are
+                        plotted.
+
+    Returns:
+        3 x num_points array of Cartesian coordinates.
+
+    Examples:
+        bowl = makeCartBowl([0, 0, 0], 1, 2, [0, 0, 1], 100)
+        bowl = makeCartBowl([0, 0, 0], 1, 2, [0, 0, 1], 100, True)
+    """
+
+    # define literals (ref: http://www.wolframalpha.com/input/?i=golden+angle)
+    GOLDEN_ANGLE = 2.39996322972865332223155550663361385312499901105811504
+
+    # check input values
+    if radius <= 0:
+        raise ValueError("The radius must be positive.")
+    if diameter <= 0:
+        raise ValueError("The diameter must be positive.")
+    if diameter > 2 * radius:
+        raise ValueError("The diameter of the bowl must be equal or less than twice the radius of curvature.")
+    if np.all(bowl_pos == focus_pos):
+        raise ValueError("The focus_pos must be different from the bowl_pos.")
+
+    # check for infinite radius of curvature, and call makeCartDisc instead
+    if np.isinf(radius):
+        # bowl = make_cart_disc(bowl_pos, diameter / 2, focus_pos, num_points, plot_bowl)
+        # return bowl
+        raise NotImplemented("make_cart_disc")
+
+    # compute arc angle from chord (ref: https://en.wikipedia.org/wiki/Chord_(geometry))
+    varphi_max = np.arcsin(diameter / (2 * radius))
+
+    # compute spiral parameters
+    theta = lambda t: GOLDEN_ANGLE * t
+    C = 2 * np.pi * (1 - np.cos(varphi_max)) / (num_points - 1)
+    varphi = lambda t: np.arccos(1 - C * t / (2 * np.pi))
+
+    # compute canonical spiral points
+    t = np.linspace(0, num_points - 1, num_points)
+    p0 = np.array([np.cos(theta(t)) * np.sin(varphi(t)),
+                   np.sin(theta(t)) * np.sin(varphi(t)),
+                   np.cos(varphi(t))])
+    p0 = radius * p0
+
+    # linearly transform the canonical spiral points to give bowl in correct orientation
+    R, b = compute_linear_transform(bowl_pos, focus_pos, radius)
+
+    if b.ndim == 1:
+        b = np.expand_dims(b, axis=-1)  # expand dims for broadcasting
+
+    bowl = R @ p0 + b
+
+    # plot results
+    if plot_bowl:
+        # select suitable axis scaling factor
+        _, scale, prefix, unit = scale_SI(np.max(bowl))
+
+        # create the figure
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(bowl[0, :] * scale, bowl[1, :] * scale, bowl[2, :] * scale)
+        ax.set_xlabel('[' + prefix + unit + ']')
+        ax.set_ylabel('[' + prefix + unit + ']')
+        ax.set_zlabel('[' + prefix + unit + ']')
+        ax.set_box_aspect([1, 1, 1])
+        plt.grid(True)
+        plt.show()
+
+    return bowl
 
 
 def get_spaced_points(start: float, stop: float, n: int = 100, spacing: str = 'linear') -> np.ndarray:
