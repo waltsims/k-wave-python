aed4c021dd9da7a422b63678bc0940cd55eccc02
David Sinden <djps@users.noreply.github.com>
2023-09-28 20:18:31 +0200

    Update filters.py (#202)
    * Update filters.py
    
    * update tests for odd and even length data
    
    * Add new toneburst
    
    * add condition for tonelength
    
    * shorten line length
    
    * Update signals.py - use matlab remainder function

--- kwave/utils/signals.py
+++ kwave/utils/signals.py
@@ -10,7 +10,7 @@ from .conversion import freq2wavenumber
 from .data import scale_SI
 from .mapgen import ndgrid
 from .math import sinc, gaussian
-from .matlab import matlab_mask, unflatten_matlab_mask
+from .matlab import matlab_mask, unflatten_matlab_mask, rem
 from .matrix import broadcast_axis, num_dim
 
 
@@ -343,8 +343,10 @@ def tone_burst(sample_freq, signal_freq, num_cycles, envelope='Gaussian', plot_s
 
     # create the tone burst
     tone_length = num_cycles / signal_freq  # [s]
-    # We want to include the endpoint but only if it's divisible by the step-size
-    if tone_length % dt < 1e-18:
+    # We want to include the endpoint but only if it's divisible by the step-size. 
+    # Modulo operator is not stable, so multiple conditions included.
+    # if ( (tone_length % dt) < 1e-18 or (np.abs(tone_length % dt - dt) < 1e-18) ):
+    if (rem(tone_length, dt) < 1e-18):
         tone_t = np.linspace(0, tone_length, int(tone_length / dt) + 1)
     else:
         tone_t = np.arange(0, tone_length, dt)
