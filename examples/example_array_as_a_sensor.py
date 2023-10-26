bb8fc31f775f3d3d8840b4651df1648e604708fd
Walter Simson <walter.simson@tum.de>
2023-10-26 11:01:42 -0700

    clean up example file after review

--- examples/example_array_as_a_sensor.py
+++ examples/example_array_as_a_sensor.py
@@ -45,7 +45,9 @@ def main():
     kgrid.makeTime(medium.sound_speed)
 
     source = kSource()
-    source.p0 = make_disc(N, Vector([N.x / 4 + 20, N.y / 4]), 4)
+    x_offset = 20  # [pixels]
+    # make a small disc in the top left of the domain
+    source.p0 = make_disc(N, Vector([N.x / 4 + x_offset, N.y / 4]), 4)
     source.p0[99:119, 59:199] = 1
     logical_p0 = source.p0.astype(bool)
     sensor = kSensor()
@@ -72,8 +74,8 @@ def main():
     # VISUALIZATION
     # =========================================================================
 
-    # create pml mask (default size in 2D is 20 grid points)
-    pml_size = 20
+    # create pml mask (re-use default size of 20 grid points from simulation_options)
+    pml_size = simulation_options.pml_x_size  # 20 [grid_points] 
     pml_mask = np.zeros((N.x, N.y), dtype=bool)
     pml_mask[:pml_size, :] = 1
     pml_mask[:, :pml_size] = 1
@@ -81,12 +83,42 @@ def main():
     pml_mask[:, -pml_size:] = 1
 
     # Plot source, sensor, and pml masks
-    fig = plt.figure()
-    plt.imshow(sensor.mask | logical_p0 | pml_mask, cmap='gray')
+
+    # Assign unique values to each mask
+    sensor_val = sensor.mask * 1
+    logical_p0_val = logical_p0 * 2
+    pml_mask_val = pml_mask * 3
+
+    # Combine masks
+    combined_mask = sensor_val + logical_p0_val + pml_mask_val
+    combined_mask = np.flipud(combined_mask) 
+
+    # Define custom colormap
+    colors = [
+        (1, 1, 1),  # White (Background)
+        (233/255, 131/255, 0/255),  # Orange (Sensor) 
+        (254/255, 221/255, 92/255),  # Yellow (Sources) 
+        (0.8, 0.8, 0.8),  # Light Grey (PML Mask)
+    ]
+    cmap = plt.matplotlib.colors.ListedColormap(colors)
+
+    fig, ax = plt.subplots()
+    c = ax.pcolormesh(combined_mask, cmap=cmap, shading='auto')
     plt.axis('image')
 
-    # Overlay the physical source positions
-    # karray.plotArray(False)
+    # Define labels for the colorbar
+    labels = {
+        0: 'None',
+        1: 'Sensor',
+        2: 'Initial pressure p0',
+        3: 'PML Mask',
+    }
+
+    colorbar = plt.colorbar(c, ticks=list(labels.keys()), ax=ax)
+    colorbar.ax.set_yticklabels(list(labels.values()))
+
+    ax.set_xticks([])
+    ax.set_yticks([])
 
     # Plot recorded sensor data
     fig, (ax1, ax2)= plt.subplots(ncols=1, nrows=2)
