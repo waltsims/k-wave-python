e4e9b5ba5a4fb74bfc523a8c907f24730650ac8f
Farid Yagubbayli <faridyagubbayli@gmail.com>
2024-01-16 11:57:00 +0100

    Update make_cart_sphere, make_circle, focused_bowl_oneil

--- kwave/utils/typing.py
+++ kwave/utils/typing.py
@@ -1,5 +1,5 @@
 from beartype.typing import Union
-from nptyping import Int, Bool, Float, NDArray, Shape
+from nptyping import Int, Bool, Float, Complex, NDArray, Shape
 import numpy as np
 
 
@@ -11,6 +11,7 @@ NUMERIC_WITH_COMPLEX = Union[int, float, complex, np.number]
 NP_ARRAY_INT_1D = NDArray[Shape["Dim1"], Int]
 NP_ARRAY_FLOAT_1D = NDArray[Shape["Dim1"], Int]
 NP_ARRAY_BOOL_1D = NDArray[Shape["Dim1"], Int]
+NP_ARRAY_COMPLEX_1D = NDArray[Shape["Dim1"], Complex]
 NP_ARRAY_INT_2D = NDArray[Shape["Dim1, Dim2"], Int]
 NP_ARRAY_BOOL_2D = NDArray[Shape["Dim1, Dim2"], Bool]
 NP_ARRAY_FLOAT_2D = NDArray[Shape["Dim1, Dim2"], Float]
