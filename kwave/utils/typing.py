8a391d16e2113615851e29c56ec6ed0793bc3d38
Farid Yagubbayli <faridyagubbayli@gmail.com>
2024-01-16 12:25:50 +0100

    WIP

--- kwave/utils/typing.py
+++ kwave/utils/typing.py
@@ -9,8 +9,8 @@ NUMERIC_WITH_COMPLEX = Union[int, float, complex, np.number]
 
 
 NP_ARRAY_INT_1D = NDArray[Shape["Dim1"], Int]
-NP_ARRAY_FLOAT_1D = NDArray[Shape["Dim1"], Int]
-NP_ARRAY_BOOL_1D = NDArray[Shape["Dim1"], Int]
+NP_ARRAY_FLOAT_1D = NDArray[Shape["Dim1"], Float]
+NP_ARRAY_BOOL_1D = NDArray[Shape["Dim1"], Bool]
 NP_ARRAY_COMPLEX_1D = NDArray[Shape["Dim1"], Complex]
 NP_ARRAY_INT_2D = NDArray[Shape["Dim1, Dim2"], Int]
 NP_ARRAY_BOOL_2D = NDArray[Shape["Dim1, Dim2"], Bool]
