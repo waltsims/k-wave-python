8dc1fa5db9076ba8b812b3f3489b42ea1d2ab7c1
Walter Simson <waltersimson@stanford.edu>
2024-02-22 09:40:03 -0800

    delete double
    Co-authored-by: Farid Yagubbayli <faridyagubbayli@users.noreply.github.com>

--- examples/us_bmode_linear_transducer/example_utils.py
+++ examples/us_bmode_linear_transducer/example_utils.py
@@ -5,7 +5,7 @@ def download_from_gdrive(file_id, output_path):
     try:
         import gdown
     except ModuleNotFoundError:
-        raise AssertionError("This example requires `gdown` to be installed. " "Please install using `pip install gdown`")
+        raise AssertionError("This example requires `gdown` to be installed. Please install using `pip install gdown`")
 
     url = f"https://drive.google.com/uc?export=download&confirm=t&id={file_id}"
     gdown.download(url, output_path, quiet=False)
