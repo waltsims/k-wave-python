3b6e73e973b06425911de49ad0e5b0c237025dd5
David Sinden <djps@users.noreply.github.com>
2024-02-26 16:28:14 +0100

    import vector

--- tests/test_pmlutils.py
+++ tests/test_pmlutils.py
@@ -1,5 +1,8 @@
 import pytest
 
+
+from kwave.data import Vector
+
 from kwave.kgrid import kWaveGrid
 
 from kwave.kmedium import kWaveMedium
@@ -9,7 +12,6 @@ from kwave.kspaceFirstOrder2D import kspaceFirstOrder2DC, kspace_first_order_2d_
 from kwave.options.simulation_execution_options import SimulationExecutionOptions
 from kwave.options.simulation_options import SimulationOptions
 from kwave.utils.mapgen import make_disc, make_cart_circle
-from tests.diff_utils import compare_against_ref
 
 from kwave.utils.pml import get_optimal_pml_size
 
