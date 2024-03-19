c1c9bd95740e61167d88a33b5306a9be28b44af9
Walter Simson <walter.simson@tum.de>
2024-03-19 11:31:54 -0700

    initial refactor of dunder init
    There is some circular logic with the binary lists and the urls etc...

--- kwave/__init__.py
+++ kwave/__init__.py
@@ -15,24 +15,29 @@ VERSION = "0.3.2"
 # This code is a crutch and should be removed when kspaceFirstOrder
 # is refactored
 
-platform = sys.platform
+# Constants and Configurations
+URL_BASE = "https://github.com/waltsims/"
+BINARY_VERSION = "v1.3.0"
+PREFIX = f"{URL_BASE}kspaceFirstOrder-{{}}-{{}}/releases/download/{BINARY_VERSION}/"
+BINARY_PATH = Path(__file__).parent / "bin"
+PLATFORM = sys.platform
 
-if platform.startswith("linux"):
+# TODO: workflow -> get binary list for current system -> if installed check if they match json file -> if they don't match json file, download them, if they do match, don't download them
+# TODO: workflow -> if not installed, download them
+if PLATFORM.startswith("linux"):
     system = "linux"
-elif platform.startswith(("win", "cygwin")):
+elif PLATFORM.startswith(("win", "cygwin")):
     system = "windows"
-elif platform.startswith("darwin"):
+elif PLATFORM.startswith("darwin"):
     system = "darwin"
     raise NotImplementedError("k-wave-python is currently unsupported on MacOS.")
 
-binary_path = os.path.join(Path(__file__).parent, "bin", system)
-environ["KWAVE_BINARY_PATH"] = binary_path
+# TODO: install directly in to /bin/ directory system directory is no longer needed
+BINARY_PATH = os.path.join(Path(__file__).parent, "bin", system)
+environ["KWAVE_BINARY_PATH"] = BINARY_PATH
 
-url_base = "https://github.com/waltsims/"
 
-prefix = "https://github.com/waltsims/kspaceFirstOrder-{0}-{1}/releases/download/v1.3.0/"
-
-common_filenames = [
+COMMON_FILENAMES = [
     ("cufft64_10.dll", None),
     ("hdf5.dll", None),
     ("hdf5_hl.dll", None),
@@ -45,29 +50,29 @@ common_filenames = [
     ("zlib.dll", None),
 ]
 
-specific_omp_filenames = [("kspaceFirstOrder-OMP.exe", "cpu")]
-specific_cuda_filenames = [("kspaceFirstOrder-CUDA.exe", "cuda")]
+SPECIFIC_OMP_FILENAMES = [("kspaceFirstOrder-OMP", "cpu")]
+SPECIFIC_CUDA_FILENAMES = [("kspaceFirstOrder-CUDA", "cuda")]
 
 
 def get_windows_release_urls(version: str, system_type: str) -> list:
     if version == "OMP":
-        specific_filenames = specific_omp_filenames
+        specific_filenames = [(SPECIFIC_OMP_FILENAMES[0][0] + ".exe", "cpu")]
     elif version == "CUDA":
-        specific_filenames = specific_cuda_filenames
+        specific_filenames = [(SPECIFIC_CUDA_FILENAMES[0][0] + ".exe", "cuda")]
     else:
         specific_filenames = []
 
     release_urls = []
-    for filename, binary_type in common_filenames + specific_filenames:
-        release_urls.append(prefix.format(version, system_type) + filename)
+    for filename, _ in COMMON_FILENAMES + specific_filenames:
+        release_urls.append(PREFIX.format(version, system_type) + filename)
     return release_urls
 
 
 # GitHub release URLs
-url_dict = {
+URL_DICT = {
     "linux": {
-        "cuda": [url_base + "kspaceFirstOrder-CUDA-linux/releases/download/v1.3.1/kspaceFirstOrder-CUDA"],
-        "cpu": [url_base + "kspaceFirstOrder-OMP-linux/releases/download/v1.3.0/kspaceFirstOrder-OMP"],
+        "cuda": [URL_BASE + f"kspaceFirstOrder-CUDA-{system}/releases/download/v1.3.1/{SPECIFIC_CUDA_FILENAMES[0][0]}"],
+        "cpu": [URL_BASE + f"kspaceFirstOrder-OMP-{system}/releases/download/{BINARY_VERSION}/{SPECIFIC_OMP_FILENAMES[0][0]}"],
     },
     # "darwin": {
     #     "cuda": [url_base + "kspaceFirstOrder-CUDA-linux/releases/download/v1.3/kspaceFirstOrder-CUDA"],
@@ -84,7 +89,7 @@ def _hash_file(filepath: str) -> str:
     buf_size = 65536  # 64kb chunks
     md5 = hashlib.md5()
 
-    with open(filepath, 'rb') as f:
+    with open(filepath, "rb") as f:
         while True:
             data = f.read(buf_size)
             if not data:
@@ -94,17 +99,17 @@ def _hash_file(filepath: str) -> str:
 
 
 def _is_binary_present(binary_name: str, binary_type: str) -> bool:
-    binary_filepath = os.path.join(binary_path, binary_name)
+    binary_filepath = os.path.join(BINARY_PATH, binary_name)
     binary_file_exists = os.path.exists(binary_filepath)
     if not binary_file_exists:
         return False
-    
+
     if binary_type is None:
         # this is non-kwave windows binary
         # it already exists according to the check above
         return True
-    
-    existing_metadata_path = os.path.join(binary_path, f'{binary_name}_metadata.json')
+
+    existing_metadata_path = os.path.join(BINARY_PATH, f"{binary_name}_metadata.json")
     if not os.path.exists(existing_metadata_path):
         # metadata does not exist => binaries may or may not exist
         # Let's play safe and claim they don't exist
@@ -114,18 +119,21 @@ def _is_binary_present(binary_name: str, binary_type: str) -> bool:
 
     # If metadata was somehow corrupted
     file_hash = _hash_file(binary_filepath)
-    if existing_metadata['file_hash'] != file_hash:
+    if existing_metadata["file_hash"] != file_hash:
         return False
-    
+
     # If there is a new binary
-    latest_url = url_dict[system][binary_type][0]
-    if existing_metadata['url'] != latest_url:
+    latest_url = URL_DICT[system][binary_type][0]
+    if existing_metadata["url"] != latest_url:
         return False
-    
+
     # No need to check `version` field for now
     # because we version is already present in the URL
+    # TODO: Workflow should be that the config file is shipped with the release
+    #       --> working assumption is the config is **always** present
+    #       --> if the version in the config file is updated, we should update the local binary
+    #       -->
     return True
-
 
 
 def binaries_present() -> bool:
@@ -135,19 +143,13 @@ def binaries_present() -> bool:
         bool, True if binaries are present, False otherwise
 
     """
-    binary_list = {
-        "linux": [
-            # "acousticFieldPropagator-OMP",
-            ("kspaceFirstOrder-OMP", "cpu"),
-            ("kspaceFirstOrder-CUDA", "cuda")
-        ],
-        "darwin": [
-            # "acousticFieldPropagator-OMP",
-            ("kspaceFirstOrder-OMP", "cpu"),
-            ("kspaceFirstOrder-CUDA", "cuda")
-        ],
-        "windows": specific_omp_filenames + specific_cuda_filenames + common_filenames,
-    }
+    binary_types = ["cpu", "cuda"]
+
+    binary_list = []
+    for binary_type in binary_types:
+        for binary_name in URL_DICT[system][binary_type]:
+            binary_list.append((binary_name.split("/")[-1], binary_type))
+
     missing_binaries: List[str] = []
 
     for binary_name, binary_type in binary_list[system]:
@@ -170,14 +172,10 @@ def _record_binary_metadata(binary_version: str, binary_filepath: str, binary_ur
     # note: version is not immediately useful at the moment
     # because it is already present in the url and we use url to understand if versions match
     # However, let's record it anyway. Maybe it will be useful in the future.
-    metadata = {
-        "url": binary_url,
-        "version": binary_version,
-        "file_hash": _hash_file(binary_filepath)
-    }
-    metadata_filename = f'{filename}_metadata.json'
-    metadata_filepath = os.path.join(binary_path, metadata_filename)
-    with open(metadata_filepath, "w") as outfile: 
+    metadata = {"url": binary_url, "version": binary_version, "file_hash": _hash_file(binary_filepath)}
+    metadata_filename = f"{filename}_metadata.json"
+    metadata_filepath = os.path.join(BINARY_PATH, metadata_filename)
+    with open(metadata_filepath, "w") as outfile:
         json.dump(metadata, outfile, indent=4)
 
 
@@ -192,25 +190,20 @@ def download_binaries(system_os: str, bin_type: str):
         None
 
     """
-    for url in url_dict[system_os][bin_type]:
+    for url in URL_DICT[system_os][bin_type]:
         # Extract the file name from the GitHub release URL
         binary_version, filename = url.split("/")[-2:]
 
-        logging.log(logging.INFO, f"Downloading {filename} to {binary_path}...")
+        logging.log(logging.INFO, f"Downloading {filename} to {BINARY_PATH}...")
 
         # Create the directory if it does not yet exist
-        os.makedirs(binary_path, exist_ok=True)
+        os.makedirs(BINARY_PATH, exist_ok=True)
 
         # Download the binary file
         try:
-            binary_filepath = os.path.join(binary_path, filename)
+            binary_filepath = os.path.join(BINARY_PATH, filename)
             urllib.request.urlretrieve(url, binary_filepath)
-            _record_binary_metadata(
-                binary_version=binary_version,
-                binary_filepath=binary_filepath,
-                binary_url=url,
-                filename=filename
-            )
+            _record_binary_metadata(binary_version=binary_version, binary_filepath=binary_filepath, binary_url=url, filename=filename)
         except TimeoutError:
             logging.log(
                 logging.WARN,
@@ -219,7 +212,7 @@ def download_binaries(system_os: str, bin_type: str):
                 "Partially downloaded files will be removed.",
             )
             try:
-                os.remove(binary_path)
+                os.remove(BINARY_PATH)
             except Exception:
                 folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bin")
                 logging.warning(
