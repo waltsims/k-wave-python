c90f53b1f16c545c457fbb0667d205af6d7a194b
Walter Simson <walter.simson@tum.de>
2024-03-18 22:46:23 -0700

    make logic more explicit

--- kwave/utils/kwave_array.py
+++ kwave/utils/kwave_array.py
@@ -409,10 +409,10 @@ class kWaveArray(object):
     def get_array_grid_weights(self, kgrid):
         self.check_for_elements()
 
-        grid_weights = np.zeros((kgrid.Nx, kgrid.Ny, max(kgrid.Nz, 1)))
-
-        if not self.dim == 3:
-            grid_weights = grid_weights.squeeze()
+        if kgrid.Nz >= 1:
+            grid_weights = np.zeros((kgrid.Nx, kgrid.Ny, kgrid.Nz))
+        else:
+            grid_weights = np.zeros((kgrid.Nx, kgrid.Ny))
 
         assert len(grid_weights.shape) == self.dim, "Grid weights shape must match kArray dimensions."
 
