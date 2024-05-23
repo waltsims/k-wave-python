1f89a79253ffde9146c22e24e9013132d0708bd5
Walter Simson <walter.simson@tum.de>
2024-05-22 22:11:48 -0700

    uncommited change

--- tests/test__init__.py
+++ tests/test__init__.py
@@ -6,4 +6,6 @@ from unittest.mock import patch
 def test__init():
     with pytest.raises(NotImplementedError):
         with patch("platform.system", lambda: "Darwin"):
+            import kwave
+
             importlib.reload(kwave)
