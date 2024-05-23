b144a83cd65ed12c718adeb6d910f4a3c71f9eeb
Walter Simson <walter.simson@tum.de>
2024-05-22 21:44:47 -0700

    test not impl. on darwin

---
+++ tests/test__init__.py
@@ -0,0 +1,9 @@
+import pytest
+from unittest.mock import patch
+
+
+def test__init():
+    with pytest.raises(NotImplementedError):
+        with patch("platform.system", lambda: "Darwin"):
+            import kwave  # noqa: F401
+        pass
