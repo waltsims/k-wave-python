ddfa30222908e41bcad6c1af88bb61a10fb192d2
Walter Simson <waltersimson@stanford.edu>
2024-06-21 11:42:39 -0700

    catch lack of GPU support on MacOS

--- kwave/executor.py
+++ kwave/executor.py
@@ -15,7 +15,12 @@ class Executor:
         self._make_binary_executable()
 
     def _make_binary_executable(self):
-        self.execution_options.binary_path.chmod(self.execution_options.binary_path.stat().st_mode | stat.S_IEXEC)
+        try:
+            self.execution_options.binary_path.chmod(self.execution_options.binary_path.stat().st_mode | stat.S_IEXEC)
+        except FileNotFoundError:
+            if kwave.PLATFORM == 'darwin' and self.execution_options.is_gpu_simulation:
+                raise ValueError("GPU simulations are currently not supported on MacOS")
+                
 
     def run_simulation(self, input_filename: str, output_filename: str, options: str):
         command = (
