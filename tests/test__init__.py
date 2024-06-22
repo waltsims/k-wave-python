2ee5ef8d9d3c75359e47f75aa4d65d48774d7a69
Walter Simson <walter.simson@tum.de>
2024-06-21 20:16:59 -0700

    use fakefs

--- tests/test__init__.py
+++ tests/test__init__.py
@@ -1,10 +1,13 @@
 import os
 import importlib
-from unittest.mock import patch
 import pytest
+from unittest.mock import patch
+
+import kwave
 
 
 @pytest.mark.parametrize("platform_name", ["Darwin"])
+@pytest.mark.usefixtures("fs")
 def test_not_implemented_error(platform_name):
     with pytest.raises(NotImplementedError):
         with patch("platform.system", return_value=platform_name):
@@ -20,8 +23,9 @@ def mock_windows_platform():
 
 
 @pytest.fixture
-def mock_download():
+def mock_download(fs):  # Use `fs` fixture from `pyfakefs`
     created_files = []
+    fs.create_dir(kwave.BINARY_PATH)
 
     def _create_file(url, file_path):
         content = "Dummy content"
@@ -32,7 +36,7 @@ def mock_download():
 
     yield _create_file
 
-    # Cleanup: Remove all created files
+    # Cleanup: Remove all created files (not necessary with pyfakefs, but kept for completeness)
     for filepath in created_files:
         try:
             os.remove(filepath)
@@ -40,7 +44,8 @@ def mock_download():
             pass
 
 
-def test_windows_dll_download_once(mock_windows_platform, mock_download):
+@pytest.mark.usefixtures("fs")
+def test_windows_dll_download_once(mock_windows_platform, mock_download):  # Use `fs` fixture
     # Patch the `urllib.request.urlretrieve` to use `mock_download`
     with patch("urllib.request.urlretrieve", side_effect=mock_download) as mock_urlretrieve:
         # First import to simulate initial download
