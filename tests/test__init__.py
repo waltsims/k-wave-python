80547ec2b6c6b31c7a9ee34e9438fa428b5e7d5b
Walter Simson <walter.simson@tum.de>
2024-06-21 19:53:57 -0700

    add clean up to tests

--- tests/test__init__.py
+++ tests/test__init__.py
@@ -1,9 +1,7 @@
-import shutil
-import pytest
+import os
 import importlib
 from unittest.mock import patch
-
-import kwave
+import pytest
 
 
 @pytest.mark.parametrize("platform_name", ["Darwin"])
@@ -21,37 +19,46 @@ def mock_windows_platform():
         yield mock
 
 
-def test_windows_dll_download_once(mock_windows_platform):
-    def side_effect(url, filename):
-        with open(filename, "wb") as f:
-            f.write(b"Dummy file contents")
+@pytest.fixture
+def mock_download():
+    created_files = []
 
-    # Mock the urllib.request.urlretrieve where it is used in the kwave module
-    with patch("urllib.request.urlretrieve", side_effect=side_effect) as mock_urlretrieve:
+    def _create_file(url, file_path):
+        content = "Dummy content"
+        with open(file_path, "w") as f:
+            f.write(content)
+        created_files.append(file_path)
+        return file_path
+
+    yield _create_file
+
+    # Cleanup: Remove all created files
+    for filepath in created_files:
+        try:
+            os.remove(filepath)
+        except FileNotFoundError:
+            pass
+
+
+def test_windows_dll_download_once(mock_windows_platform, mock_download):
+    # Patch the `urllib.request.urlretrieve` to use `mock_download`
+    with patch("urllib.request.urlretrieve", side_effect=mock_download) as mock_urlretrieve:
         # First import to simulate initial download
         import kwave
 
         importlib.reload(kwave)
 
-        # Assert urlretrieve was called during the initial import
+        # Assert `urlretrieve` was called during the initial import
         mock_urlretrieve.assert_called()
 
         # Reset mock to clear previous call history
         mock_urlretrieve.reset_mock()
 
-        # Reload kwave to check if urlretrieve is not called again
+        # Reload `kwave` to check if `urlretrieve` is not called again
         importlib.reload(kwave)
 
-        # Assert urlretrieve was not called again
+        # Assert `urlretrieve` was not called again
         mock_urlretrieve.assert_not_called()
-
-
-# Clean up the bin directory after tests
-def test_clean_up():
-    try:
-        shutil.rmtree(kwave.BINARY_PATH)
-    except FileNotFoundError:
-        pass
 
 
 if __name__ == "__main__":
