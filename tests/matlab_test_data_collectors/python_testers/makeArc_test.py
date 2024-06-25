ddb1ffccb7a6d9650ff15e29e4ef541730424535
Walter Simson <walter.a.simson@gmail.com>
2024-06-24 20:07:35 -0700

    update makeArc test

--- tests/matlab_test_data_collectors/python_testers/makeArc_test.py
+++ tests/matlab_test_data_collectors/python_testers/makeArc_test.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+import pytest
 from kwave.data import Vector
 from kwave.utils.mapgen import make_arc
 
@@ -8,13 +11,13 @@ from tests.matlab_test_data_collectors.python_testers.utils.record_reader import
 
 
 def test_makeArc():
-    reader = TestRecordReader("collectedValues/makeArc.mat")
+    reader = TestRecordReader(os.path.join(Path(__file__).parent, "collectedValues/makeArc.mat"))
 
     for i in range(len(reader)):
         logging.log(logging.INFO, i)
 
         grid_size, arc_pos, radius, diameter, focus_pos = reader.expected_value_of("params")
-        grid_size, arc_pos, diameter, focus_pos = grid_size[0], arc_pos[0], int(diameter), focus_pos[0]
+        grid_size, arc_pos, diameter, focus_pos = grid_size, arc_pos, int(diameter), focus_pos
         try:
             radius = int(radius)
         except OverflowError:
@@ -29,3 +32,7 @@ def test_makeArc():
         assert np.allclose(expected_arc, arc)
 
     logging.log(logging.INFO, "make_arc(..) works as expected!")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
