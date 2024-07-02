56888326c2bc55d923ccd63035cc03ec12b369ea
Walter Simson <waltersimson@stanford.edu>
2024-07-02 11:43:43 -0700

    clean up print

--- tests/matlab_test_data_collectors/python_testers/test_resize.py
+++ tests/matlab_test_data_collectors/python_testers/test_resize.py
@@ -20,7 +20,6 @@ def test_resize():
 
         resized_volume = resize(volume, new_size, interp_mode=method)
 
-        print(i)
         assert np.allclose(expected_resized_volume, resized_volume), f"Results do not match for {i + 1} dimensional case."
         reader.increment()
 
