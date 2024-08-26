36e35ac3bee0e2b34c61f06d7e7c06aafe905052
Walter Simson <walter.a.simson@gmail.com>
2024-08-26 16:16:09 -0700

    use f-strings for log messages

--- kwave/kWaveSimulation_helper/display_simulation_params.py
+++ kwave/kWaveSimulation_helper/display_simulation_params.py
@@ -13,7 +13,7 @@ def display_simulation_params(kgrid: kWaveGrid, medium: kWaveMedium, elastic_cod
     k_size = kgrid.size
 
     # display time step information
-    logging.log(logging.INFO, "  dt: ", f"{scale_SI(dt)[0]}s, t_end: {scale_SI(t_array_end)[0]}s, time steps:", Nt)
+    logging.log(logging.INFO, f"  dt: {scale_SI(dt)[0]} s, t_end: {scale_SI(t_array_end)[0]}s, time steps: {Nt}")
 
     c_min, c_min_comp, c_min_shear = get_min_sound_speed(medium, elastic_code)
 
@@ -67,31 +67,24 @@ def print_max_supported_freq(kgrid, c_min):
 
     if kgrid.dim == 1:
         # display maximum supported frequency
-        logging.log(logging.INFO, "  maximum supported frequency: ", scale_SI(k_max_all * c_min / (2 * np.pi))[0], "Hz")
+        logging.log(logging.INFO, f"  maximum supported frequency: {scale_SI(k_max_all * c_min / (2 * np.pi))[0]} Hz")
 
     elif kgrid.dim == 2:
         # display maximum supported frequency
         if k_max.x == k_max.y:
-            logging.log(logging.INFO, "  maximum supported frequency: ", scale_SI(k_max_all * c_min / (2 * np.pi))[0], "Hz")
+            logging.log(logging.INFO, f"  maximum supported frequency: {scale_SI(k_max_all * c_min / (2 * np.pi))[0]} Hz")
         else:
             logging.log(
                 logging.INFO,
-                "  maximum supported frequency: ",
-                scale_SI(k_max.x * c_min / (2 * np.pi))[0],
-                "Hz by ",
-                scale_SI(k_max.y * c_min / (2 * np.pi))[0],
-                "Hz",
+                f"  maximum supported frequency: {scale_SI(k_max.x * c_min / (2 * np.pi))[0]} Hz by {scale_SI(k_max.y * c_min / (2 * np.pi))[0]} Hz",
             )
 
     elif kgrid.dim == 3:
         # display maximum supported frequency
         if k_max.x == k_max.z and k_max.x == k_max.y:
-            logging.log(logging.INFO, "  maximum supported frequency: ", f"{scale_SI(k_max_all * c_min / (2*np.pi))[0]}Hz")
+            logging.log(logging.INFO, f"  maximum supported frequency: {scale_SI(k_max_all * c_min / (2*np.pi))[0]} Hz")
         else:
             logging.log(
                 logging.INFO,
-                "  maximum supported frequency: ",
-                f"{scale_SI(k_max.x * c_min / (2*np.pi))[0]}Hz by "
-                f"{scale_SI(k_max.y * c_min / (2*np.pi))[0]}Hz by "
-                f"{scale_SI(k_max.z * c_min / (2*np.pi))[0]}Hz",
+                f"  maximum supported frequency: {scale_SI(k_max.x * c_min / (2*np.pi))[0]} Hz by {scale_SI(k_max.y * c_min / (2*np.pi))[0]} Hz by {scale_SI(k_max.z * c_min / (2*np.pi))[0]} Hz",
             )
