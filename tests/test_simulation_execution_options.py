16d23c154e1401d2906d4aa5fb90ef8d4de9968a
Walter Simson <waltersimson@stanford.edu>
2024-11-21 20:13:46 -0800

    Deactivate number of threads (-t) option on windows (#514)
    * add failing test
    
    * add bugfix
    
    * update tests to run on windows
    
    * exclude -t for windows

--- tests/test_simulation_execution_options.py
+++ tests/test_simulation_execution_options.py
@@ -86,7 +86,35 @@ class TestSimulationExecutionOptions(unittest.TestCase):
             options = SimulationExecutionOptions()
             self.assertTrue(options.binary_name.endswith(".exe"))
 
-    def test_get_options_string(self):
+    @patch("kwave.options.simulation_execution_options.PLATFORM", "darwin")
+    def test_get_options_string_darwin(self):
+        """Test the get_options_string method with a mock sensor."""
+        options = self.default_options
+        options.device_num = 1
+        options.num_threads = os.cpu_count()
+        options.verbose_level = 2
+
+        options_string = options.get_options_string(self.mock_sensor)
+        expected_substrings = [" -g 1", f" -t {os.cpu_count()}", " --verbose 2", " --p_raw", " --u_max", " -s 10"]
+        for substring in expected_substrings:
+            self.assertIn(substring, options_string)
+
+    @patch("kwave.options.simulation_execution_options.PLATFORM", "windows")
+    def test_get_options_string_windows(self):
+        """Test the get_options_string method with a mock sensor."""
+        options = self.default_options
+        options.device_num = 1
+        options.num_threads = os.cpu_count()
+        options.verbose_level = 2
+
+        options_string = options.get_options_string(self.mock_sensor)
+        expected_substrings = [" -g 1", " --verbose 2", " --p_raw", " --u_max", " -s 10"]
+        for substring in expected_substrings:
+            self.assertIn(substring, options_string)
+        self.assertNotIn(f" -t {os.cpu_count()}", expected_substrings)
+
+    @patch("kwave.options.simulation_execution_options.PLATFORM", "linux")
+    def test_get_options_string_linux(self):
         """Test the get_options_string method with a mock sensor."""
         options = self.default_options
         options.device_num = 1
