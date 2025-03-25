06d8ddf2f3c9ad56c3a5abdad287c5176b1486cc
Walter Simson <waltsims@users.noreply.github.com>
2025-03-25 01:04:53 -0700

    [WIP] Update plane recording

--- examples/pr_3D_TR_planar_sensor/pr_3D_TR_planar_sensor.py
+++ examples/pr_3D_TR_planar_sensor/pr_3D_TR_planar_sensor.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from kwave.data import Vector
 from kwave.kgrid import kWaveGrid
@@ -25,7 +26,9 @@ from kwave.utils.mapgen import make_ball
 
 
 def main():
+    # --------------------
     # SIMULATION
+    # --------------------
 
     # change scale to 2 to reproduce the higher resolution figures used in the
     # help file
@@ -68,6 +71,7 @@ def main():
     # Create sensor mask in main domain
     sensor.mask = np.zeros((Nx, Ny, Nz))
     sensor.mask[0, :, :] = 1  # Set first x-plane to 1
+    sensor.record = ["p", "p_final"]
 
     # Now pad the grid with PML
     pad_size = ((PML_size, PML_size), (PML_size, PML_size), (PML_size, PML_size))
@@ -110,82 +114,125 @@ def main():
     # apply a positivity condition
     p0_recon[p0_recon < 0] = 0
 
+    # --------------------
     # VISUALIZATION
+    # --------------------
+
     cmap = get_color_map()
     plot_scale = [-10, 10]
 
     # plot the initial pressure
     fig, axs = plt.subplots(2, 2)
-    axs[0, 0].imshow(
+
+    # x-y plane
+    im = axs[0, 0].imshow(
         p0[:, :, N[2] // 2],
         extent=[kgrid.y_vec.min() * 1e3, kgrid.y_vec.max() * 1e3, kgrid.x_vec.max() * 1e3, kgrid.x_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[0, 0])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[0, 0].set_title("x-y plane")
+    axs[0, 0].set_ylabel("x-position [mm]")
+    axs[0, 0].set_xlabel("y-position [mm]")
     axs[0, 0].axis("image")
 
-    axs[0, 1].imshow(
+    # x-z plane
+    im = axs[0, 1].imshow(
         p0[:, N[1] // 2, :],
         extent=[kgrid.z_vec.min() * 1e3, kgrid.z_vec.max() * 1e3, kgrid.x_vec.max() * 1e3, kgrid.x_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[0, 1])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[0, 1].set_title("x-z plane")
+    axs[0, 1].set_ylabel("x-position [mm]")
+    axs[0, 1].set_xlabel("z-position [mm]")
     axs[0, 1].axis("image")
 
-    axs[1, 0].imshow(
+    # y-z plane
+    im = axs[1, 0].imshow(
         p0[N[0] // 2, :, :],
         extent=[kgrid.z_vec.min() * 1e3, kgrid.z_vec.max() * 1e3, kgrid.y_vec.max() * 1e3, kgrid.y_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[1, 0])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[1, 0].set_title("y-z plane")
+    axs[1, 0].set_ylabel("y-position [mm]")
+    axs[1, 0].set_xlabel("z-position [mm]")
     axs[1, 0].axis("image")
 
     axs[1, 1].axis("off")
     axs[1, 1].set_title("(All axes in mm)")
 
+    plt.tight_layout()
     plt.show()
 
     # plot the reconstructed initial pressure
     fig, axs = plt.subplots(2, 2)
-    axs[0, 0].imshow(
+
+    # x-y plane
+    im = axs[0, 0].imshow(
         p0_recon[:, :, N[2] // 2],
         extent=[kgrid.y_vec.min() * 1e3, kgrid.y_vec.max() * 1e3, kgrid.x_vec.max() * 1e3, kgrid.x_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[0, 0])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[0, 0].set_title("x-y plane")
+    axs[0, 0].set_ylabel("x-position [mm]")
+    axs[0, 0].set_xlabel("y-position [mm]")
     axs[0, 0].axis("image")
 
-    axs[0, 1].imshow(
+    # x-z plane
+    im = axs[0, 1].imshow(
         p0_recon[:, N[1] // 2, :],
         extent=[kgrid.z_vec.min() * 1e3, kgrid.z_vec.max() * 1e3, kgrid.x_vec.max() * 1e3, kgrid.x_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[0, 1])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[0, 1].set_title("x-z plane")
+    axs[0, 1].set_ylabel("x-position [mm]")
+    axs[0, 1].set_xlabel("z-position [mm]")
     axs[0, 1].axis("image")
 
-    axs[1, 0].imshow(
+    # y-z plane
+    im = axs[1, 0].imshow(
         p0_recon[N[0] // 2, :, :],
         extent=[kgrid.z_vec.min() * 1e3, kgrid.z_vec.max() * 1e3, kgrid.y_vec.max() * 1e3, kgrid.y_vec.min() * 1e3],
         vmin=plot_scale[0],
         vmax=plot_scale[1],
         cmap=cmap,
     )
+    divider = make_axes_locatable(axs[1, 0])
+    cax = divider.append_axes("right", size="3%", pad="2%")
+    fig.colorbar(im, cax=cax)
     axs[1, 0].set_title("y-z plane")
+    axs[1, 0].set_ylabel("y-position [mm]")
+    axs[1, 0].set_xlabel("z-position [mm]")
     axs[1, 0].axis("image")
 
     axs[1, 1].axis("off")
     axs[1, 1].set_title("(All axes in mm)")
 
+    plt.tight_layout()
     plt.show()
 
 
