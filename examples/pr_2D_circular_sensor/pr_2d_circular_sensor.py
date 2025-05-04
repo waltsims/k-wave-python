e66192cef2a5021d9d44756799005b3fd6749590
David Sinden <djps@users.noreply.github.com>
2025-05-04 22:26:47 +0200

    linting test

--- examples/pr_2D_circular_sensor/pr_2d_circular_sensor.py
+++ examples/pr_2D_circular_sensor/pr_2d_circular_sensor.py
@@ -1,32 +1,14 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
-
-from matplotlib import colors
-from matplotlib.animation import FuncAnimation
-
-# from scipy.interpolate import RegularGridInterpolator
-from copy import deepcopy
-# from termcolor import colored
-
 import requests
-import io # Import the io module
-import cv2 # Import OpenCV for image decoding
-
+import io
+import cv2 
 import sys
 import os
-
-sys.path.remove("C:\\Users\\dsinden\\GitHub\\k-wave-python")
-
-
-# Add the directory of the current file to sys.path
-two_levels_up = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
-
-sys.path.append(two_levels_up)
-
-# print(two_levels_up)
-# print(sys.path)
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+from matplotlib import colors
+from matplotlib.animation import FuncAnimation
+from copy import deepcopy
 
 from kwave.data import Vector
 from kwave.utils.conversion import cart2grid
@@ -43,14 +25,9 @@ from kwave.utils.signals import add_noise, reorder_binary_sensor_data
 from kwave.utils.colormap import get_color_map
 from kwave.utils.matrix import resize, sort_rows
 from kwave.kspaceFirstOrder2D import kspaceFirstOrder2D, kspaceFirstOrder2DC
-
 from kwave.options.simulation_options import SimulationOptions
 from kwave.options.simulation_execution_options import SimulationExecutionOptions
-
 from kwave.reconstruction.time_reversal import TimeReversal
-
-verbose = False
-
 
 pml_size: int = 20 # size of the PML in grid points
 Nx: int = 256 - 2 * pml_size # number of grid points in the x direction
@@ -68,7 +45,7 @@ medium = kWaveMedium(sound_speed=1500.0)
 
 kgrid.makeTime(medium.sound_speed)
 
-p0_magnitude = 2
+p0_magnitude = 2.0
 p0 = p0_magnitude * load_image('tests/EXAMPLE_source_two.bmp', is_gray=True)
 
 p0 = resize(p0, [Nx, Ny])
@@ -148,7 +125,6 @@ cart_sensor_mask_reordered = cart_sensor_mask[:, np.squeeze(reorder_index.T).ast
 sensor_data_reordered = reorder_binary_sensor_data(sensor_data_original['p'].T, reorder_index=reorder_index)
 
 fig, ax = plt.subplots(1, 1)
-
 im = ax.pcolormesh(np.squeeze(kgrid.x_vec)* 1e3, 
                    np.squeeze(kgrid.y_vec)* 1e3, p0, shading='gouraud', cmap=cmap, vmin=-1, vmax=1)
 ax.scatter(cart_sensor_mask[1, :] * 1e3, cart_sensor_mask[0, :] * 1e3, c='k', marker='o', s=8)
@@ -177,7 +153,9 @@ cbar.ax.tick_params(labelsize=8)
 
 # add noise to the recorded sensor data
 signal_to_noise_ratio: float = 40.0	# [dB]
+
 sensor_data = add_noise(sensor_data_original['p'].T, signal_to_noise_ratio, 'peak')
+
 
 # create a second computation grid for the reconstruction to avoid the
 # inverse crime
@@ -206,26 +184,8 @@ sensor.recorded_pressure = sensor_data
 # set the record type: record the pressure waveform
 sensor.record = ['p']
 
-tr = TimeReversal(kgrid_recon, medium, sensor)
+tr = TimeReversal(kgrid_recon, medium, sensor, compensation_factor=1.0)
 p0_recon = tr(kspaceFirstOrder2D, simulation_options, execution_options)
-
-#######
-
-
-
-# # reset the initial pressure, ensures source has p0, but it has no effect
-# source.p0 = np.zeros_like(kgrid_recon.x)
-
-# # assign the time reversal data. Not reversed
-# sensor.time_reversal_boundary_data = sensor_data.T
-
-# # run the time-reversal reconstruction
-# p0_recon = kspaceFirstOrder2D(medium=deepcopy(medium),
-                              # kgrid=deepcopy(kgrid_recon),
-                              # source=deepcopy(source),
-                              # sensor=deepcopy(sensor),
-                              # simulation_options=deepcopy(simulation_options),
-                              # execution_options=deepcopy(execution_options))
 
 fig, ax = plt.subplots()
 im = plt.pcolormesh(np.squeeze(kgrid_recon.x_vec), 
@@ -239,38 +199,32 @@ ax.set_title('Reconstructed Pressure Distribution')
 # THIRD
 # =========================================================================
 
-# create a binary sensor mask of an equivalent continuous circle, this has the enlarged number of sensor points
+# create a binary sensor mask of an equivalent continuous circle, 
+# this has the enlarged number of sensor points
 sensor_radius_grid_points: int = round(sensor_radius / kgrid_recon.dx)
-binary_sensor_mask = make_circle(Vector([Nx, Ny]), Vector([Nx // 2, Ny // 2]), sensor_radius_grid_points, sensor_angle)
+binary_sensor_mask = make_circle(Vector([Nx, Ny]), Vector([Nx // 2, Ny // 2]),
+                                 sensor_radius_grid_points, sensor_angle)
 binary_sensor_mask = binary_sensor_mask.astype(bool)
 
 del sensor
 sensor = kSensor()
 sensor.mask = binary_sensor_mask
+sensor.record = ['p']
+sensor.recorded_pressure = interp_cart_data(kgrid=kgrid_recon,
+                                            cart_sensor_data=sensor_data_reordered,
+                                            cart_sensor_mask=cart_sensor_mask,
+                                            binary_sensor_mask=binary_sensor_mask)
 
-sensor.time_reversal_boundary_data = interp_cart_data(kgrid=kgrid_recon,
-                                                      cart_sensor_data=sensor_data_reordered,
-                                                      cart_sensor_mask=cart_sensor_mask,
-                                                      binary_sensor_mask=binary_sensor_mask)
-
-# # sensor defines the source
-# tr = TimeReversal(kgrid_recon, medium, sensor)
-# p0_recon_interp = tr(kspaceFirstOrder2D, simulation_options, execution_options)
-
-p0_recon_interp = kspaceFirstOrder2D(medium=deepcopy(medium),
-                                     kgrid=deepcopy(kgrid_recon),
-                                     source=deepcopy(source),
-                                     sensor=deepcopy(sensor),
-                                     simulation_options=deepcopy(simulation_options),
-                                     execution_options=deepcopy(execution_options))
+# sensor defines the source
+tr = TimeReversal(kgrid_recon, medium, sensor, compensation_factor=1.0)
+p0_recon_interp = tr(kspaceFirstOrder2D, simulation_options, execution_options)
 
 fig, ax = plt.subplots()
 im = plt.pcolormesh(np.squeeze(kgrid_recon.x_vec), 
-                    np.squeeze(kgrid_recon.y_vec), p0_recon_interp['p_final'].T, 
+                    np.squeeze(kgrid_recon.y_vec), p0_recon_interp, 
                     cmap=cmap, vmin=-1, vmax=1)
 ax.yaxis.set_inverted(True)
 ax.set_title('Reconstructed Pressure Distribution with Interpolation')
-
 
 # plot a profile for comparison
 slice_pos = 4.5e-3;  # [m] location of the slice from top of grid [m]
@@ -279,10 +233,8 @@ j = int(round(slice_pos / kgrid_recon.dx))
 fig, ax = plt.subplots()
 ax.plot(np.squeeze(kgrid.y_vec) * 1e3, p0[i,: ], 'k--', label='Initial Pressure')
 ax.plot(np.squeeze(kgrid_recon.y_vec) * 1e3, np.transpose(p0_recon)[:, j], 'r-', label='Point Reconstruction')
-ax.plot(np.squeeze(kgrid_recon.y_vec) * 1e3, p0_recon_interp['p_final'][:, j], 'b-', label='Interpolated Reconstruction')
+ax.plot(np.squeeze(kgrid_recon.y_vec) * 1e3, np.transpose(p0_recon_interp)[:, j], 'b-', label='Interpolated Reconstruction')
 ax.set_xlabel('y-position [mm]')
 ax.set_ylabel('Pressure')
 ax.set_ylim(0, 2.1)
 ax.legend()
-
-plt.show()
