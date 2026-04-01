fb0c0e31dcbaaac10b09fc62cb5fabafc7276f69
Walter Simson <waltsims@users.noreply.github.com>
2026-04-01 01:31:18 -0700

    Revert "Revert direct attr access back to getattr (moving to separate PR)"
    This reverts commit 73b558413fc0112de33b01bef0e5221eef02914f.

--- kwave/solvers/kspace_solver.py
+++ kwave/solvers/kspace_solver.py
@@ -411,7 +411,7 @@ class Simulation:
 
         # Absorption/dispersion
         alpha_coeff_raw = getattr(self.medium, "alpha_coeff", 0)
-        alpha_mode = getattr(self.medium, "alpha_mode", None)
+        alpha_mode = self.medium.alpha_mode
         if not _is_enabled(alpha_coeff_raw):
             self._absorption = lambda div_u: 0
             self._dispersion = lambda rho: 0
